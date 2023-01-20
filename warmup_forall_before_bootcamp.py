670025dde447394509e2d742e11b4e54b45b1c4e
Daud Khan <109561961+daudkhan2024@users.noreply.github.com>
2023-01-20 22:53:51 +0100

    Add files via upload

---
+++ warmup_forall_before_bootcamp.py
@@ -0,0 +1,24 @@
+# warmup before bootcamp
+
+print("There once was a man name jhon")
+print("he was 60 years old")
+print("he like the name micheal")
+print("but he didnt like being 99")
+
+# an approach to change name and age any time iny place
+
+character_name = "aslam"
+character_age = "30"
+print("There once was a man name " + character_name + " ")
+print("he was " + character_age + " years old .")
+
+character_name ="ahmad"
+print("He like the name " + character_name + ", ")
+print("but he didnt like being " + character_age + ". ")
+
+
+
+
+
+
+
